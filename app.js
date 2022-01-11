2f253a0170d319db8a91279a911e80581e872570
Alexandre Fabian <xandy.fabian@gmail.com>
2022-01-11 18:59:13 -0300

    Removido transparencia menu topo com scrool

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ hamburger.addEventListener('click', () => {
 	mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
+//Trecho comentado para remover mudança de cor no fundo do menu de topo.
+//document.addEventListener('scroll', () => {
+//	var scroll_position = window.scrollY;
+//	if (scroll_position > 250) {
+//		header.style.backgroundColor = '#29323c';
+//	} else {
+//		header.style.backgroundColor = 'transparent';
+//	}
+//});
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
